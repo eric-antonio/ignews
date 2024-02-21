fe85bd8e2946cff59f8598c5a684f98f38934633
eric-antonio <ericantoniodev@gmail.com>
2024-02-21 21:49:57 +0200

    Home section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import Head from "next/head";
-
+import styles from "./home.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
-        <title> Inicio | ig.news</title>
+        <title> Home | ig.news</title>
       </Head>
-
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span className={styles.priceSpan}> for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/mulher.svg" alt="GirlCoding" />
+      </main>
     </>
   );
 }
